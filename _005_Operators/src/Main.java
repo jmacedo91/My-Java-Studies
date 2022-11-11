40163bbe02aa8732560baf98c08ecfa3cad28d35
jmacedo91 <jonathan.silvamacedo@gmail.com>
2022-11-11 20:40:44 -0300

    if-then Statement

--- _005_Operators/src/Main.java
+++ _005_Operators/src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
     public static void main(String[] args) {
 
         int result = 1 + 2; // 1 + 2 = 3
@@ -43,7 +44,10 @@ public class Main {
         result -= 2; // 10 - 2 = 8
         System.out.println("10 - 2 = " + result);
 
-
-
+        boolean isAlien = false;
+        if (!isAlien) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I scared of aliens");
+        }
     }
 }
